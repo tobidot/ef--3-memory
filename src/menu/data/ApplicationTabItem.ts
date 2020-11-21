8b29053bd9aafd1505e232d6e52ca03ecbce456b
tobidot <Object.name@live.de>
2020-11-21 21:40:21 +0100

    remove react remnants

--- src/menu/data/ApplicationTabItem.ts
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export interface ApplicationWindowItem<STATE> {
-    state: STATE;
-    tab: React.ReactElement<{
-        onClick?: (...args: any[]) => any
-    }>;
-    content: React.ReactNode;
-}
