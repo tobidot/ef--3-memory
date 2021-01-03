28d742c715d4105070d26e9006c3ab1a0dd6bcf8
tobidot <Object.name@live.de>
2021-01-03 19:38:54 +0100

    adding comments

--- src/game/views/main/MainView.ts
+++ src/game/views/main/MainView.ts
@@ -1,9 +1,9 @@
-import {tools} from "@game.object/ts-game-toolbox";
-import {CanvasView} from "@game.object/ts-game-toolbox/dist/src/abstract/mvc/CanvasView";
-import {RgbColor} from "@game.object/ts-game-toolbox/dist/src/data/RgbColor";
-import {ChainProperty} from "@game.object/ts-game-toolbox/dist/src/signals/ChainProperty";
-import {ViewCollection} from "../ViewCollection";
-import {Vector2, Vector2I} from "@game.object/ts-game-toolbox/dist/src/geometries/Vector2";
+import { tools } from "@game.object/ts-game-toolbox";
+import { CanvasView } from "@game.object/ts-game-toolbox/dist/src/abstract/mvc/CanvasView";
+import { RgbColor } from "@game.object/ts-game-toolbox/dist/src/data/RgbColor";
+import { ChainProperty } from "@game.object/ts-game-toolbox/dist/src/signals/ChainProperty";
+import { ViewCollection } from "../ViewCollection";
+import { Vector2, Vector2I } from "@game.object/ts-game-toolbox/dist/src/geometries/Vector2";
 
 interface ViewStarAttr {
     position: Vector2I;
@@ -13,14 +13,19 @@ interface ViewStarAttr {
 }
 
 export class MainView extends CanvasView<ViewCollection> {
+    /// Color of the "sky"
     public bg_color = new ChainProperty<this, RgbColor>(this, tools.commons.Colors.BLACK);
-    public fg_color = new ChainProperty<this, RgbColor>(this, tools.commons.Colors.WHITE);
+    /// All stars on the screen
     public stars = new ChainProperty<this, Array<ViewStarAttr>>(this, []);
 
     public draw(): void {
         this.reset_canvas_state();
         this.stars.get().forEach((star) => {
-            const real_position = new Vector2(star.position).mul(33 / (star.z )).add({x: 400, y: 300});
+            /**
+             * Divide the position by `z` to create a sense of depth,
+             * when the stars approach the viewer. 
+             */
+            const real_position = new Vector2(star.position).mul(33 / (star.z)).add({ x: 400, y: 300 });
             const size = star.size * 100 / star.z;
             this.context.strokeStyle = star.color.to_hex();
             this.context.fillStyle = star.color.to_hex();
@@ -28,6 +33,9 @@ export class MainView extends CanvasView<ViewCollection> {
         });
     }
 
+    /**
+     * Reset default canvas state and paint the background
+     */
     protected reset_canvas_state() {
         super.reset_canvas_state();
         this.context.fillStyle = this.bg_color.get().to_hex();
